8e1f3a124c4ca988ed24fd697ca217617ca0de8f
Logan Hurst <loganhurst2015@gmail.com>
2024-05-15 22:03:06 -0400

    Initial Commit

---
+++ HomePage.aspx
@@ -0,0 +1,120 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="Classroom_Manager_System.HomePage" %>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Jura:wght@700&display=swap" rel="stylesheet">
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        #BtAddStudent{
+            background-color: #34BA57
+        }
+        #BtAddStudent:hover{
+            background-color: #8A95A5;
+            cursor: pointer;
+        }
+        #BtSortAsc {
+            background-color: #34BA57;
+
+        }
+        #BtSortAsc:hover{
+            background-color: #8A95A5;
+            cursor: pointer;
+        }
+        #BtSortDesc {
+            background-color: #34BA57;
+        }
+        #BtSortDesc:hover{
+            background-color: #8A95A5;
+            cursor: pointer;
+        }
+        #DDLClass:hover{
+           
+            cursor: pointer;
+        }
+        .auto-style1 {
+            margin-left: 4px;
+        }
+
+        .auto-style2 {
+            width: 145px;
+        }
+        .auto-style4 {
+            margin-left: 3px;
+        }
+        .auto-style5 {
+            margin-left: 2px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1 style="font-family: 'Abril Fatface', serif;
+font-family: 'Jura', sans-serif; color: #34BA57; text-shadow:
+    -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000; ">&nbsp&nbsp&nbsp&nbsp Classroom Manager</h1>
+        <table>
+            <tr>
+                <td id="td1" class="auto-style2">Class Name:</td>
+                <td id="td2">
+                   
+                    <asp:DropDownList ID="DDLClass" OnSelectedIndexChanged = "DropDownIndexChange_TextChanged" runat="server" AutoPostBack="True" AppendDataBoundItems="true" CssClass="auto-style1" Width="209px" DataSourceID="SqlDataSource1" DataTextField="CourseName" DataValueField="CourseName">
+                        <asp:ListItem Value="0" Text="-- Select Class --" ></asp:ListItem>
+                    </asp:DropDownList>
+
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ClassroomManagerSystemConnectionString %>" ProviderName="<%$ ConnectionStrings:ClassroomManagerSystemConnectionString.ProviderName %>" SelectCommand="SELECT [CourseName] FROM [CourseTable]"></asp:SqlDataSource>
+                </td>
+            </tr>
+            
+            <tr height="5px" id="tr2">
+                <td id="td3" class="auto-style2"></td>
+                <td id="td4"></td>
+            </tr>
+            
+            <tr>
+                <td id="td5" class="auto-style2">Teacher:</td>
+                <td class="Ctd1">
+                    <asp:TextBox ID="TeacherTextBox" runat="server" Width="209px" OnTextChanged="TeacherTextBox_TextChanged" CssClass="auto-style4"></asp:TextBox></td>
+            </tr>
+            <tr id="tr4">
+                <td class="auto-style2"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="auto-style2"></td>
+                <td class="Ctd1">
+                    <asp:TextBox ID="AddStudentTextBox" runat="server" Width="209px" CssClass="auto-style4"></asp:TextBox></td>
+                <td class="Ctd1">
+                    <asp:Button ID="BtAddStudent" runat="server" Text="Add Student" OnClick="BtAddStudent_Click" /></td>
+
+            </tr>
+            <tr id="tr6">
+                <td class="auto-style2"></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td class="auto-style2"></td>
+                <td class="Ctd1">
+                    <asp:TextBox ID="StudentListTextBox" runat="server" Name="tbMultiLine" TextWrapping="Wrap" AcceptsReturn="True" VerticalScrollBarVisibility="Visible" Width="209px" Height="236px" OnTextChanged="StudentListTextBox_TextChanged1" TextMode="MultiLine" CssClass="auto-style5"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td class="auto-style2"></td>
+                <td>
+                    <table>
+                        <tr>
+                            <td id="td17" class="Ctd1">
+                                <asp:Button ID="BtSortAsc" runat="server" Text="Sort Asc" OnClick="BtSortAsc_Click" /></td>
+                            <td id="td18" class="Ctd1">
+                                <asp:Button ID="BtSortDesc" runat="server" Text="Sort Desc" OnClick="BtSortDesc_Click" /></td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ HomePage.aspx.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Classroom_Manager_System
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Method or function 
+        //Return type is before method name. int returns number, string returns text, void returns  nothing , bool returns t/f
+        protected void DDLClassName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "select * from [dbo].[CourseTable] where CourseName=@CourseName";
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlcomm.Parameters.AddWithValue("@CourseName", DDLClass.SelectedItem.Text);
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            while (sdr.Read())
+            {
+                //int courseId = Convert.int32(sdr
+            }
+            sqlconn.Close();
+
+            //Join statement
+
+
+
+            // TeacherTextBox.Text = TeacherTextBox.Text + sdr2.ToString();
+        }
+
+
+        protected void BtAddStudent_Click(object sender, System.EventArgs e)
+        {
+            
+
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+
+            string sqlquery = "Insert into [dbo].[StudentTable] (StudentName) values (@StudentName)";
+            
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlconn.Open();
+            sqlcomm.Parameters.AddWithValue("@StudentName", AddStudentTextBox.Text);
+            sqlcomm.ExecuteNonQuery();
+            
+            int StudentId = GetStudentid(sqlconn);
+            //var StudCourse = AddStudentTextBox.Text;
+            int GetCourseId = GetCourseid(sqlconn);
+            
+            string sqlquery2 = "Insert into [dbo].[StudentCourseTable] (Studentid, Courseid) values (@StudentId, @GetCourseId)";
+            SqlCommand sqlcomm2 = new SqlCommand(sqlquery2, sqlconn);
+
+            sqlcomm2.Parameters.AddWithValue("@StudentId", StudentId);
+            sqlcomm2.Parameters.AddWithValue("@GetCourseId", GetCourseId);
+
+            sqlcomm2.ExecuteNonQuery();
+            sqlconn.Close();
+        }
+        protected int GetCourseid(SqlConnection sqlconn)
+        {
+            //@ is the parameter (Select Cid from coursetable where the course name is "MATH" --> Parameter passed
+            string sqlquery = "select Cid from [dbo].[CourseTable] where CourseName = @CourseName";
+
+            int courseID;
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            //Parameter. On the right of the comma is the value being passed. This is the text of the html element id
+            sqlcomm.Parameters.AddWithValue("@CourseName", DDLClass.SelectedItem.Text);
+            //Reads sql command
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            if (sdr.Read())
+            {
+                //Convert the Cid to an integer to use later.
+                courseID = Convert.ToInt32(sdr[0]);
+
+            }
+            //Return the integer
+            return Convert.ToInt32(sdr[0]);
+            
+
+
+
+
+
+        }
+        //Pass in parameter / argument  Ex: sqlconn     
+        protected int GetStudentid(SqlConnection sqlconn)
+        {
+            string sqlquery = "select Stid from [dbo].[StudentTable] where StudentName = @StudentName";
+
+            int courseID;
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+
+            sqlcomm.Parameters.AddWithValue("@StudentName", AddStudentTextBox.Text);
+            SqlDataReader sdr = sqlcomm.ExecuteReader();
+            if (sdr.Read())
+            {
+                courseID = Convert.ToInt32(sdr[0]);
+            }
+            return Convert.ToInt32(sdr[0]);
+
+        }
+        protected void DropDownIndexChange_TextChanged(object sender, EventArgs e)
+        {
+
+
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+
+
+            TeacherTextBox1_TextChanged(sqlconn);
+
+            StudentListBox_TextChanged(sqlconn);
+            sqlconn.Close();
+
+
+        }
+
+
+
+
+        protected string TeacherTextBox1_TextChanged(SqlConnection sqlconn)
+        {
+            if (sqlconn is null)
+            {
+                throw new ArgumentNullException(nameof(sqlconn));
+            }
+
+
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+
+            //courseId represents the integer fetched from this method/ function
+            int courseId = GetCourseid(sqlconn);
+
+            //the course id above is bing used on line 144 to be passed into @Courseid to get the teacher name from the sql query below
+            string sqlquery2 = "SELECT TeacherName FROM [dbo].[TeacherTable] INNER JOIN [dbo].[TeacherCourseTable] ON [dbo].[TeacherTable].Tid = [dbo].[TeacherCourseTable].Teacherid INNER JOIN [dbo].[CourseTable] ON [dbo].[TeacherCourseTable].Courseid = [dbo].[CourseTable].Cid WHERE [dbo].[CourseTable].Cid = @Courseid;";
+
+            //We want to run sqlquery2 using sqlconn... sqlconn is the parameter being passed in
+            SqlCommand sqlcomm2 = new SqlCommand(sqlquery2, sqlconn);
+            //course Id form line 138 is being passed in as the value for the parameter @Courseid
+            sqlcomm2.Parameters.AddWithValue("@Courseid", courseId);
+
+            //We set the  html element with the id of TeacherTextBox's vallue to the TeacherName returned from the sql query
+            TeacherTextBox.Text = (string)sqlcomm2.ExecuteScalar();
+
+            //we can set this function equal to a string somewhere else because the value is being returned here Ex: string teachername = TeacherTextBox1_TextChanged(sqlconn)
+            return TeacherTextBox.Text;
+        }
+
+
+
+
+
+
+
+
+        protected string StudentListBox_TextChanged(SqlConnection sqlconn)
+        {
+            //Connection being passed in above as parameter
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            StudentListTextBox.Text = "";
+            int courseId = GetCourseid(sqlconn);
+            string sqlquery3 = "SELECT StudentName FROM [dbo].[StudentTable] INNER JOIN [dbo].[StudentCourseTable] ON [dbo].[StudentTable].Stid = [dbo].[StudentCourseTable].Studentid INNER JOIN [dbo].[CourseTable] ON [dbo].[StudentCourseTable].Courseid = [dbo].[CourseTable].Cid WHERE [dbo].[CourseTable].Cid = @Courseid;";
+            //Hover over built in functions to see parameters needed to run...
+            SqlCommand sqlcomm3 = new SqlCommand(sqlquery3, sqlconn);
+            sqlcomm3.Parameters.AddWithValue("@Courseid", courseId);
+            //sdr will be the student names from the executed query
+            SqlDataReader sdr = sqlcomm3.ExecuteReader();
+            while (sdr.Read())
+            {
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    StudentListTextBox.Text += sdr[i].ToString();
+                    // \n adds a new line
+                    StudentListTextBox.Text = StudentListTextBox.Text + '\n';
+
+                }
+            }
+
+
+            return mainconn;
+
+        }
+
+        protected void BtSortAsc_Click(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            StudentListTextBox.Text = "";
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+            int courseId = GetCourseid(sqlconn);
+            string sqlquery = "SELECT StudentName FROM [dbo].[StudentTable] JOIN [dbo].[StudentCourseTable] ON [dbo].[StudentTable].Stid = [dbo].[StudentCourseTable].Studentid JOIN [dbo].[CourseTable] ON [dbo].[StudentCourseTable].Courseid = [dbo].[CourseTable].Cid WHERE [dbo].[CourseTable].Cid = @Courseid ORDER BY StudentName ASC";
+
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlcomm.Parameters.AddWithValue("@Courseid", courseId);
+            SqlDataReader sdr2 = sqlcomm.ExecuteReader();
+            while (sdr2.Read())
+            {
+                for (int i = 0; i < sdr2.FieldCount; i++)
+                {
+                    StudentListTextBox.Text += sdr2[i].ToString();
+                    StudentListTextBox.Text = StudentListTextBox.Text + '\n';
+
+                }
+            }
+
+            // StudentListBox_TextChanged(sdr[i].ToString());
+            sqlconn.Close();
+
+            
+
+
+        }
+
+        protected void BtSortDesc_Click(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            StudentListTextBox.Text = "";
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+            int courseId = GetCourseid(sqlconn);
+            string sqlquery = "SELECT StudentName FROM [dbo].[StudentTable] JOIN [dbo].[StudentCourseTable] ON [dbo].[StudentTable].Stid = [dbo].[StudentCourseTable].Studentid JOIN [dbo].[CourseTable] ON [dbo].[StudentCourseTable].Courseid = [dbo].[CourseTable].Cid WHERE [dbo].[CourseTable].Cid = @Courseid ORDER BY StudentName DESC";
+
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlcomm.Parameters.AddWithValue("@Courseid", courseId);
+            SqlDataReader sdr2 = sqlcomm.ExecuteReader();
+            while (sdr2.Read())
+            {
+                for (int i = 0; i < sdr2.FieldCount; i++)
+                {
+                    StudentListTextBox.Text += sdr2[i].ToString();
+                    StudentListTextBox.Text = StudentListTextBox.Text + '\n';
+
+                }
+            }
+
+            // StudentListBox_TextChanged(sdr[i].ToString());
+            sqlconn.Close();
+
+        }
+
+
+        protected void TeacherTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void StudentListTextBox_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
